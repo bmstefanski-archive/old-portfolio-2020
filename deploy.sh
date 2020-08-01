a624ac2fe3dc5696800e5bc8d778cf5a10522337
bmstefanski <bstefanskicom@gmail.com>
2020-08-01 19:40:03 +0200

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 yarn build
-git push origin `git subtree split --prefix dist master`:gh-pages --force
+git push origin `git subtree split --prefix dist origin master`:gh-pages --force
